098065d58fd4ee5fc177d9b8e2683a77b1e9b22b
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-22 23:16:58 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,32 @@
+#User function Template for python3
 
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        #union
+        se = set()
+        for i in range(0,n):
+            se.add(a[i])
+        for i in range(0,m):
+            se.add(b[i])
+        return(len(se))        
+        
+            
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
